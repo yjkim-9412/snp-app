7ab8fe97ac8a68903e7e44022952c18666c3df53
YoungJun Kim <xxt1205@gmail.com>
2023-03-07 17:09:48 +0900

    front 폴더 제거

--- front/src/interface/PropsAction.ts
+++
@@ -1,8 +0,0 @@
-import {ChangeEvent, MouseEventHandler, SyntheticEvent} from "react";
-
-interface PropsAction extends ChangeEvent<HTMLInputElement>{
-    target: HTMLInputElement & EventTarget,
-}
-
-
-export default PropsAction;
