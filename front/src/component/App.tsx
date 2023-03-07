e516ed3f394240e8ecb3c5f42c324480182e9319
xxt1205 <xxt1205@gmail.com>
2023-03-07 16:47:41 +0900

    axios 불필요한 HTTP 프로토콜 헤더 정보 제거, gitignore 수정

--- front/src/component/App.tsx
+++ front/src/component/App.tsx
@@ -1,11 +1,5 @@
-import React, {useEffect, useState} from 'react';
-import axios from "axios";
-import Router from "./Router";
 import AppRouter from "./Router";
 import "bootstrap/dist/css/bootstrap.css";
-import '../axios.config';
-import { useNavigate } from 'react-router-dom';
-import {Container} from "@mui/material/";
 
 function App() {
 
--- front/src/index.tsx
+++ front/src/index.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './component/App';
 import "bootstrap/dist/css/bootstrap.css";
-import './axios.config';
 import {BrowserRouter} from "react-router-dom";
 
 const root = ReactDOM.createRoot(
